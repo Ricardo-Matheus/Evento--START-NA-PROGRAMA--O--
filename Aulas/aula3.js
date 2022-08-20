8430c5884f20ebb22d5cdc33e4d12d6adac7f153
Ricardo-Matheus <ricardomatheusramos@gmail.com>
2022-08-20 13:49:57 -0300

    ajustes

--- Aulas/aula3.js
+++ Aulas/aula3.js
@@ -1,35 +1,37 @@
 function calculo_da_gorjeta(event) {
 
-event.preventDefault();
+    event.preventDefault();
 
-let valor_da_conta = document.getElementById('conta').value;
+    let valor_da_conta = document.getElementById('conta').value;
 
-let qualidade_do_serviço = document.getElementById('serviço').value;
+    let qualidade_do_serviço = document.getElementById('serviço').value;
 
-let quantidade_de_pessoas = document.getElementById('divisao').value;
+    let quantidade_de_pessoas = document.getElementById('divisao').value;
 
-if(valor_da_conta == '' | valor_da_conta == 0 | qualidade_do_serviço == '' | qualidade_do_serviço == 0 | quantidade_de_pessoas == 0) {
+    if(valor_da_conta == '' | valor_da_conta == 0 | qualidade_do_serviço == '' | qualidade_do_serviço == 0 | quantidade_de_pessoas == 0) {
 
-    alert('PorFavor, Preencha os Campos !!');
+        alert('PorFavor, Preencha os Campos !!');
 
-    return;
-}
+        return;
+    }
 
-let resultado_da_divisão = (valor_da_conta * qualidade_do_serviço)/quantidade_de_pessoas;
+    let resultado_da_divisão = (valor_da_conta * qualidade_do_serviço)/quantidade_de_pessoas;
 
-resultado_da_divisão.toFixed(2);
+    resultado_da_divisão.toFixed(2);
 
-document.getElementById('resultado').innerHTML = resultado_da_divisão;
+    document.getElementById('resultado').innerHTML = resultado_da_divisão;
 
-document.getElementById('resultado').style.display = 'inline-block'
+    document.getElementById('resultado').style.display = 'inline-block'
 
-document.getElementById('caixa-do-resultado').style.display = 'block'
+    document.getElementById('caixa-do-resultado').style.display = 'block'
 
-if (quantidade_de_pessoas > 1) {
-    document.getElementById('cada').style.display = 'block';
-}else {
-    document.getElementById('cada').style.display = 'none'
-}
+    if (quantidade_de_pessoas > 1) {
+        document.getElementById('cada').style.display = 'block';
+    }else {
+        document.getElementById('cada').style.display = 'none'
+    }
+
+    document.getElementById('main').style.height = '500px'
 }
 
 document.getElementById('cada').style.display = 'none'
